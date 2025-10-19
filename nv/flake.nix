fdb39c2c597b9c5fe10cdb509ff604fa6f6f7f3c
Richard Stöckl <richard.stoeckl@ait.ac.at>
2025-10-19 19:16:23 +0200

    Add LSP

--- nv/flake.nix
+++ nv/flake.nix
@@ -1,43 +1,71 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf.url = "github:notashelf/nvf";
+    nvf.url = "github:notashelf/nvf/v0.8";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.x86_64-linux = {
-      # Set the default package to the wrapped instance of Neovim.
-      # This will allow running your Neovim configuration with
-      # `nix run` and in addition, sharing your configuration with
-      # other users in case your repository is public.
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux;
-          modules = [
-            {
-              config.vim = {
-                # Enable custom theming options
-                theme.enable = true;
+  outputs =
+    { nixpkgs, ... }@inputs:
+    {
+      packages.x86_64-linux = {
+        # Set the default package to the wrapped instance of Neovim.
+        # This will allow running your Neovim configuration with
+        # `nix run` and in addition, sharing your configuration with
+        # other users in case your repository is public.
+        default =
+          (inputs.nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.x86_64-linux;
+            modules = [
+              {
+                config.vim = {
+                  # Enable custom theming options
+                  theme.enable = true;
 
-                # Enable Treesitter
-                treesitter.enable = true;
+                  # Enable Treesitter
+                  treesitter.enable = true;
 
-                # Other options will go here. Refer to the config
-                # reference in Appendix B of the nvf manual.
-                # ...
+                  # Other options will go here. Refer to the config
+                  # reference in Appendix B of the nvf manual.
+                  # ...
 
-                languages = {
-                  rust.enable = true;
-                  nix.enable = true;
-                  python.enable = true;
-                  markdown.enable = true;
-                  html.enable = true;
+                  git.enable = true;
+
+                  languages = {
+                    rust = {
+                      enable = true;
+                    };
+                    go = {
+                      enable = true;
+                    };
+
+                    nix = {
+                      enable = true;
+                    };
+                    python = {
+                      enable = true;
+                    };
+                    lua = {
+                      enable = true;
+                    };
+
+                    markdown = {
+                      enable = true;
+                    };
+                    html = {
+                      enable = true;
+                    };
+                  };
+
+                  lsp = {
+                    enable = true;
+                    formatOnSave = true;
+                    inlayHints.enable = true;
+                    lspSignature.enable = true;
+                  };
                 };
-              };
-            }
-          ];
-        })
-        .neovim;
+              }
+            ];
+          }).neovim;
+      };
     };
-  };
 }
