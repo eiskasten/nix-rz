ac41d9cb53ee507c6ef257292d90cb2d7f81297b
Richard Stöckl <richard.stoeckl@aon.at>
2026-04-29 23:14:07 +0200

    Include config for kurti wlan

--- modules/features/network.nix
+++ modules/features/network.nix
@@ -1,6 +1,6 @@
 { self, inputs, ... }:
 {
-  flake.nixosModules.networking =
+  flake.nixosModules.network =
     { config, pkgs, ... }:
     {
       programs = {
@@ -18,5 +18,43 @@
 
       services.openssh.enable = true;
       networking.firewall.enable = true;
+      sops.secrets."net/wifi/kurti-wlan" = { };
+      networking.networkmanager.ensureProfiles.profiles = {
+        kurti-wlan = {
+          connection = {
+            id = "kurti-wlan";
+            type = "wifi";
+          };
+
+          wifi = {
+            mode = "infrastructure";
+            ssid = "Kurti Wlan";
+          };
+
+          wifi-security = {
+            key-mgmt = "wpa-psk";
+          };
+
+          ipv4 = {
+            dns = "1.1.1.1";
+            method = "auto";
+          };
+
+          ipv6 = {
+            addr-gen-mode = "stable-privacy";
+            method = "auto";
+          };
+        };
+      };
+      networking.networkmanager.ensureProfiles.secrets.entries = [
+        {
+          file = config.sops.secrets."net/wifi/kurti-wlan".path;
+          key = "psk";
+          matchId = "Kurti Wlan";
+          matchSetting = "wifi-security";
+          matchType = "wifi";
+        }
+
+      ];
     };
 }
