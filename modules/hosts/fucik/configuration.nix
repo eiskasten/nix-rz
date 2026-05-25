9bc2be8c155bff51f66daa9ccbe78ea756a2c0a9
Richard Stöckl <richard.stoeckl@aon.at>
2026-05-25 16:56:35 +0200

    Add disko configuration for fucik

--- modules/hosts/fucik/configuration.nix
+++ modules/hosts/fucik/configuration.nix
@@ -13,6 +13,7 @@ in
     {
       imports = [
         self.nixosModules.fucikHardware
+        self.nixosModules.fucikDisko
         # add features here
 
         self.nixosModules.myHomeManager
--- modules/hosts/fucik/hardware.nix
+++ modules/hosts/fucik/hardware.nix
@@ -30,37 +30,6 @@
           ];
         };
       };
-      fileSystems."/" = {
-        device = "/dev/disk/by-label/${rootLabel}";
-        fsType = "btrfs";
-        options = [ "subvol=@system" ];
-      };
-
-      fileSystems."/home" = {
-        device = "/dev/disk/by-label/${rootLabel}";
-        fsType = "btrfs";
-        options = [ "subvol=@home" ];
-      };
-
-      fileSystems."/swap" = {
-        device = "/dev/disk/by-label/${rootLabel}";
-        fsType = "btrfs";
-        options = [ "subvol=@swap" ];
-      };
-
-      fileSystems."/boot" = {
-        device = "/dev/disk/by-label/${bootLabel}";
-        fsType = "vfat";
-        options = [
-          "fmask=0022"
-          "dmask=0022"
-        ];
-      };
-
-      boot.initrd.luks.devices."home-richi".device =
-        "/dev/disk/by-uuid/5e96e7d1-c056-4da9-aa64-c156f148727f";
-
-      swapDevices = [ ];
       nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
     };
 }
