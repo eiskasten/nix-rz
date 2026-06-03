2e3ae4ff14b13be14a016d0f1afabe9c916977f8
Richard Stöckl <richard.stoeckl@ait.ac.at>
2026-06-03 17:56:07 +0200

    Add office profiles

--- modules/home/common/sway/kanshi.nix
+++ modules/home/common/sway/kanshi.nix
@@ -27,6 +27,94 @@
           }
 
           {
+
+            output = {
+              criteria = "Dell Inc. DELL U2722D FPJM8H3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "OFFICE_CENTER";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2722D GTJM8H3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "OFFICE_RIGHT";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2722D 3MLN6P3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "MATTHIAS_CENTER";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2722D 8ZLN6P3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "MATTHIAS_RIGHT";
+            };
+          }
+          {
+            output = {
+              criteria = "Dell Inc. DELL U3415W F1T1W9CC0PPL";
+              mode = "3440x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "DAVID_CENTER";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2722D DQJM8H3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "90";
+              alias = "DAVID_RIGHT";
+            };
+          }
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2421E G2P39P3";
+              mode = "1920x1200";
+              scale = 1.0;
+              alias = "TEL_BIG";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2724D 2L8F834";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "normal";
+              alias = "LENNY_CENTER";
+            };
+          }
+
+          {
+            output = {
+              criteria = "Dell Inc. DELL U2722D 7VN7WN3";
+              mode = "2560x1440";
+              scale = 1.0;
+              transform = "270";
+              alias = "LENNY_LEFT";
+            };
+          }
+
+          {
             profile = {
               name = "kempfendorf";
               outputs = [
@@ -39,6 +127,112 @@
                   criteria = "$KEMPFENDORF_CENTER";
                   position = "0,0";
                   status = "enable";
+                }
+              ];
+            };
+          }
+
+          {
+            profile = {
+              name = "office";
+              outputs = [
+                {
+                  criteria = "$INTERNAL";
+                  status = "enable";
+                }
+                {
+                  criteria = "$OFFICE_CENTER";
+                  status = "enable";
+                  position = "1920,0";
+                }
+                {
+                  criteria = "$OFFICE_RIGHT";
+                  status = "enable";
+                  position = "4480,0";
+                }
+              ];
+            };
+          }
+
+          {
+            profile = {
+              name = "matthias";
+              outputs = [
+                {
+                  criteria = "$INTERNAL";
+                  status = "enable";
+                }
+                {
+                  criteria = "$MATTHIAS_CENTER";
+                  status = "enable";
+                  position = "1920,0";
+                }
+                {
+                  criteria = "$MATTHIAS_RIGHT";
+                  status = "enable";
+                  position = "4480,0";
+                }
+              ];
+            };
+          }
+
+          {
+            profile = {
+              name = "david";
+              outputs = [
+                {
+                  criteria = "$INTERNAL";
+                  status = "enable";
+                }
+                {
+                  criteria = "$DAVID_CENTER";
+                  status = "enable";
+                  position = "1920,0";
+                }
+                {
+                  criteria = "$DAVID_RIGHT";
+                  status = "enable";
+                  position = "5360,-670";
+                }
+              ];
+            };
+          }
+
+          {
+            profile = {
+              name = "lenny";
+              outputs = [
+                {
+                  criteria = "$INTERNAL";
+                  status = "enable";
+                }
+                {
+                  criteria = "$LENNY_CENTER";
+                  status = "enable";
+                  position = "-2560,0";
+                }
+                {
+                  criteria = "$LENNY_LEFT";
+                  status = "enable";
+                  position = "-4000,-670";
+                }
+              ];
+            };
+          }
+
+          {
+            profile = {
+              name = "telbig";
+              outputs = [
+                {
+                  criteria = "$INTERNAL";
+                  status = "enable";
+                  position = "0,1200";
+                }
+                {
+                  criteria = "$TEL_BIG";
+                  status = "enable";
+                  position = "-2560,0";
                 }
               ];
             };
