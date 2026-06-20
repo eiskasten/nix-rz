b28df0577fae6e5ddf976bfd985dda8a9e8134cd
Richard Stöckl <richard.stoeckl@aon.at>
2026-06-20 13:50:49 +0200

    Enable pass otp extension

--- modules/home/common/pass.nix
+++ modules/home/common/pass.nix
@@ -7,6 +7,7 @@
       programs.password-store.enable = true;
       programs.password-store.settings = {
       };
+      programs.password-store.package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
 
       programs.browserpass.enable = true;
     };
